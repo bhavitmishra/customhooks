5ad2915b135c03191136cb6ffeb2f17eefdc7cda
BhavitMishra <bhavitmishra@gmail.com>
2025-07-02 15:33:02 +0530

    This is a custom hook which does data fetching and updation every n seconds

---
+++ frontendcustomhooks/vite-project/src/App.jsx
@@ -0,0 +1,56 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import axios from "axios";
+
+function useTodos(n) {
+  const [todos, setTodos] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const value = setInterval(() => {
+      axios.get("http://localhost:5000/todos").then((res) => {
+        setTodos(res.data);
+        setLoading(false);
+      });
+    }, n * 1000);
+    axios.get("http://localhost:5000/todos").then((res) => {
+      setTodos(res.data);
+      setLoading(false);
+    });
+    return () => {
+      clearInterval(value);
+    };
+  }, [n]);
+  return { todos, loading };
+}
+
+function App() {
+  const { todos, loading } = useTodos(3);
+  if (loading) {
+    console.log("fucked");
+
+    return <div>loading...</div>;
+  }
+
+  return (
+    <div>
+      {todos.map((todo) => (
+        <Track todo={todo} />
+      ))}
+    </div>
+  );
+}
+
+function Track({ todo }) {
+  return (
+    <div key={todo.userid}>
+      {todo.id}
+      <br />
+      {todo.title}
+      <br />
+      {todo.completed}
+    </div>
+  );
+}
+
+export default App;
